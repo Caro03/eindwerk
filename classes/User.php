8c78edc5b9242628ae582ff622829a5a51a082ec
Caro03 <caroverstreken@hotmail.com>
2021-07-02 17:04:04 +0200

    Added login & register

---
+++ classes/User.php
@@ -0,0 +1,231 @@
+<?php
+
+include_once(__DIR__ . "/Db.php");
+
+class User {
+        private $userID;
+        private $email;
+        private $password;
+        private $firstname;
+        private $lastname;
+        private $role;
+
+        public function getUserID()
+        {
+                return $this->userID;
+        }
+
+        public function setUserID($userID)
+        {
+                $this->userID = $userID;
+
+                return $this;
+        }
+
+        public function setEmail($email) {
+
+                if(empty($email)) {
+                        throw new Exception("Email mag niet leeg zijn!");
+                }
+                $this->email = $email;
+
+                return $this;
+        }
+
+        public function getEmail() {
+                return $this->email;
+        }
+
+        public function setPassword($password) {
+
+            if(empty($password)) {
+                    throw new Exception("Wachtwoord mag niet leeg zijn!");
+            }
+
+            return $this;
+        }
+
+        public function getPassword() {
+            return $this->password;
+        }
+
+        public function setFirstname($firstname) {
+            $this->firstname = $firstname;
+
+            return $this;
+        }
+
+        public function getFirstname(){
+            return $this->firstname;
+        }
+
+        public function setLastname($lastname) {
+                $this->lastname = $lastname;
+    
+                return $this;
+        }
+    
+        public function getLastname(){
+                return $this->lastname;
+        }
+
+        public function setRole($role){
+                $this->role = $role;
+
+                return $this;
+        }
+
+        public function getRole() {
+                return $this->role;
+        }
+
+
+        public function validateEmail($email)
+        {
+            // Remove all illegal characters from email
+            $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    
+            // validate e-mail + check for Thomas More email address
+            if (filter_var($email, FILTER_VALIDATE_EMAIL) && preg_match('|@student.thomasmore.be$|', $email)) {
+                return true;
+            } else {
+                echo false;
+            }
+        }
+
+        public function emailAvailable($email)
+        {
+            $pdo = Db::connect();
+            $stmt = $pdo->prepare("SELECT COUNT(id) FROM users WHERE email = :email");
+            $stmt->bindParam(':email', $email);
+            $stmt->execute();
+            $result = $stmt->fetchColumn();
+    
+            if ($result > 0) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        public function validatePassword($password)
+        {
+            $length = strlen($password);
+    
+            if ($length < 5) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        public function register($email, $password, $firstname, $lastname, $role)
+        {
+            $pdo = Db::connect();
+            $stmt = $pdo->prepare("INSERT INTO users (firstname, lastname, email, password, role_id) VALUES (:firstname, :lastname, :email, :password, :role)");
+            $password = password_hash($password, PASSWORD_DEFAULT, ['cost' => 12]);
+            $stmt->bindParam(':firstname', $firstname);
+            $stmt->bindParam(':lastname', $lastname);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':password', $password);
+            $stmt->bindParam(':role', $role);
+            $result = $stmt->execute();
+            return $result;
+        }
+
+        public function fetchUserID($email)
+        {
+            $pdo = Db::connect();
+            $stmt = $pdo->prepare("SELECT id FROM users WHERE email = :email");
+            $stmt->bindParam(':email', $email);
+            $stmt->execute();
+            $result = $stmt->fetchColumn();
+            return $result;
+        }
+
+        public function checkLogin($email, $password) {
+            //db conn
+            $conn = Db::connect();
+            //insert query
+            $statement = $conn->prepare("select * from users where email = :email");
+            $statement->bindParam(":email", $email);
+    
+            //return result
+            $statement->execute();
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            if(empty($result)) {
+                return false;
+            }
+            $hash = $result["password"];
+            if(password_verify($password, $hash)) {
+                return true;
+            }else {
+                return false;
+            }
+        }
+
+        public function userIDFromSession($email) {
+            //db conn
+            $conn = Db::connect();
+            //insert query
+            $statement = $conn->prepare("select id from users where email = :email");
+            $statement->bindParam(":email", $email);
+    
+            //return result
+            $statement->execute();
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            return $result;
+        }
+
+        public function fetchRole($userID){
+            $conn = Db::connect();
+            $statement = $conn->prepare('SELECT role_id FROM users WHERE id = :userID');
+            $statement->bindParam(':userID', $userID);
+            $statement->execute();
+            $result = $statement->fetch(PDO::FETCH_COLUMN);
+            return $result;
+        }
+  
+        public function allUserData()
+        {
+            //db conn
+            $conn = Db::connect();
+            //insert query
+            $statement = $conn->prepare("select * from users where id = :userID");
+            $userID = $this->getUserID();
+            $statement->bindParam(':userID', $userID);
+
+            //return result
+            $statement->execute();
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            
+            return $result;
+        }
+        public function fetchPData($userID){
+            $conn = Db::connect();
+            $statement = $conn->prepare('SELECT * FROM users WHERE id = :userID');
+            $statement->bindParam(':userID', $userID);
+            $statement->execute();
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            return $result;
+        }
+
+        public function changeEmail($email, $userID){
+            $conn = Db::connect();
+            $statement = $conn->prepare("UPDATE `users` SET `email` = :email WHERE `id` = :userID");
+            $statement->bindParam(':userID', $userID);
+            $statement->bindParam(':email', $email);
+            $result = $statement->execute();
+        return $result;
+        }
+
+        public function changePass($password, $userID){
+            $conn = Db::connect();
+            $statement = $conn->prepare("UPDATE `users` SET `password` = :password WHERE `id` = :userID");
+            $password = password_hash($password, PASSWORD_DEFAULT, ['cost' => 12]);
+            $statement->bindParam(':userID', $userID);
+            $statement->bindParam(':password', $password);
+            $result = $statement->execute();
+        return $result;
+        }
+}
---
+++ register.php
@@ -0,0 +1,142 @@
+<?php
+include_once(__DIR__ . "/classes/Db.php");
+include_once(__DIR__ . "/classes/User.php");
+session_start();
+session_destroy();
+
+// if register form is submitted and not empty
+if (!empty($_POST['register'])) {
+    if (count(array_filter($_POST)) == count($_POST)) {
+        // check if email is filled out
+        if (!empty($_POST['email'])) {
+            // check for thomas more email
+            $verifyEmail = new User();
+            $email = $_POST['email'];
+            $verifyEmail->setEmail($email);
+            $resultEmail = $verifyEmail->validateEmail($email);
+
+            // if thomas more email = ok
+            if ($resultEmail == 1) {
+                // check if email is not taken
+                $emailAvailable = new User();
+                $emailAvailable->setEmail($email);
+                $available = $emailAvailable->emailAvailable($email);
+                if ($available == 1) {
+                    // check if password and verifypassword are the same
+                    if (!empty($_POST['password']) && $_POST['password'] === $_POST['verifyPassword']) {
+                        //check if password length is ok
+                        $verifyPassword = new User();
+                        $password = $_POST['password'];
+                        $verifyPassword->setPassword($password);
+                        $resultPassword = $verifyPassword->validatePassword($password);
+
+                        if ($resultPassword == 1) {
+                            // register the user
+                            $user = new User();
+                            $firstname = $_POST['firstname'];
+                            $lastname = $_POST['lastname'];
+                            $role = $_POST['role'];
+                            $email = $_POST['email'];
+                            $password = $_POST['password'];
+                            $user->setFirstname($firstname);
+                            $user->setLastname($lastname);
+                            $user->setRole($role);
+                            $user->setEmail($email);
+                            $user->setPassword($password);
+                            $register = $user->register($email, $password, $firstname, $lastname, $role);
+
+                            session_start();
+                            $userID = $user->fetchUserID($email);
+                            $_SESSION['user'] = $userID;
+                            header("Location: index.php");
+                        } else {
+                            $error = "Password too short.";
+                            $alert = 5;
+                        }
+                    } else {
+                        $error = "Password doesn't match.";
+                        $alert = 4;
+                    }
+                } else {
+                    $error = "Email taken.";
+                    $alert = 3;
+                }
+            } else {
+                $error = "Only Thomas More emails please.";
+                $alert = 2;
+            }
+        }
+    } else {
+        $error = "Fill all fields out please.";
+        $alert = 1;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="build/tailwind.css">
+    <title>Register</title>
+</head>
+
+<body class="h-screen gradient">
+    <div class="h-auto pb-10 mx-5 my-10 ml-auto mr-auto bg-white rounded-3xl w-60 sm:w-80 md:w-96">
+        <img class="pt-5 mb-2 ml-auto mr-auto logo" src="./images/logo-slogan.png">
+        <div class="container">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+
+                <h2 class="mb-10 text-xl text-center form_title md:text-2xl">Registreren</h2>
+
+                <?php if (isset($error)) : ?>
+                    <div class="mb-5 text-center form_error">
+                        <p class="form_error">
+                            <?php echo $error; ?>
+                        </p>
+                    </div>
+                <?php endif; ?>
+
+                <div class="form_field">
+                    <input class="block mb-8 ml-auto mr-auto bg-transparent border-b border-black w-52 sm:w-64 form_field md:w-72" placeholder="First name" type="text" name="firstname" id="firstname">
+                </div>
+
+                <div class="form_field">
+                    <input class="block mb-8 ml-auto mr-auto bg-transparent border-b border-black w-52 sm:w-64 form_field md:w-72" placeholder="Last name" type="text" name="lastname" id="lastname">
+                </div>
+
+                <div class="form_field">
+                    <input class="block mb-8 ml-auto mr-auto bg-transparent border-b border-black w-52 sm:w-64 form_field md:w-72" placeholder="Email" type="text" name="email" id="email">
+                </div>
+
+                <div class="form_field">
+                    <input class="block mb-8 ml-auto mr-auto bg-transparent border-b border-black w-52 sm:w-64 form_field md:w-72" placeholder="Password" type="password" name="password" id="password">
+                </div>
+
+                <div class="form_field">
+                    <input class="block mb-8 ml-auto mr-auto bg-transparent border-b border-black w-52 sm:w-64 form_field md:w-72" placeholder="Verify password" type="password" name="verifyPassword" id="verifyPassword">
+                </div>
+
+                <div class="block mb-8 ml-auto mr-auto w-52 sm:w-64 radio-item form_field md:w-72">
+                    <label class="mr-2" for="role">Ik ben een...</label>
+                    <input type="radio" id="student" name="role" value="2">
+                    <label class="mr-2" for="student">Student</label>
+                    <input type="radio" id="docent" name="role" value="1">
+                    <label for="docent">Docent</label>
+                </div>
+
+                <div class="form_button">
+                    <input class="block h-12 mb-2 ml-auto mr-auto text-white shadow-md w-52 sm:w-64 form_btn md:w-72 rounded-2xl" type="submit" value="Registreren" name="register" id="register">
+                </div>
+            </form>
+            <div class="text-sm text-center">
+                <a class="form_register" href="login.php">Al een account? Log je hier in</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
